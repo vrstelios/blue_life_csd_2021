fd53c9d9a4d660c38baaee4668ac6f1f976d3323
Konstantinos Gallos <kggallos@it.auth.gr>
2021-06-10 00:38:50 +0300

    Προσθήκη error προφίλ χωρίς δικαιώματα πρόσβασης για μη συνδεδεμένο χρήστη

---
+++ UnauthorizedProfile.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Blue Life - Μη εξουσιοδοτημένο προφίλ</title>
+    <link rel="icon" href="images/Main/BlueLife-icon.ico">
+    <link rel="stylesheet" href="styles_main.css">
+    <link rel="stylesheet" href="styles_profile.css">
+</head>
+<body>
+<header id="header">
+    <h1>Blue Life</h1>
+</header>
+
+<!---------------Navigation bar--------------->
+
+<!---------------Title section--------------->
+<div class="page-title">
+    <div class='vidContain'>
+        <div class='vid'>
+
+        </div>
+        <h4>Δεν έχετε πρόσβαση στα στοιχεία. Παρακαλούμε συνδεθείτε πρώτα.</h4>
+    </div>
+</div>
+
+</body>
+</html>
