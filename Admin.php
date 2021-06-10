9b5af3159dae4f4cf0d50602dab9750d762b93aa
evasilog <evasilog@csd.auth.gr>
2021-06-10 05:07:38 +0300

    διόρθωση στις φόρμες "δημιουργία χρήστη" και "δημιουργία ", προσθήκη πίνακα "επικοινωνία χρηστών"

--- Admin.php
+++ Admin.php
@@ -6,6 +6,105 @@
     <link rel="icon" href="images/Main/BlueLife-icon.ico">
     <link rel="stylesheet" href="styles_main.css">
     <link rel="stylesheet" href="styles_admin.css">
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $link = 1; // άχρηστη γραμμή κώδικα, απλά για να μην εμφανίζει error στην μεταβλητή $link παρακάτω
+        include("connect_to_database.php");
+        echo $_POST['submit'];
+        if ($_POST['submit'] == 'Καταχώρηση χρήστη'){
+            if (isset($_POST['username'])) {
+                $username = $_POST['username'];
+            } else {
+                $username = null;
+            }
+            if (isset($_POST['email'])) {
+                $email = $_POST['email'];
+            } else {
+                $email = null;
+            }
+            if (isset($_POST['firstname'])) {
+                $firstname = $_POST['firstname'];
+            } else {
+                $firstname = null;
+            }
+            if (isset($_POST['lastname'])) {
+                $lastname = $_POST['lastname'];
+            } else {
+                $lastname = null;
+            }
+            if (isset($_POST['pass'])) {
+                $password = $_POST['pass'];
+            } else {
+                $password = null;
+            }
+
+            $query = "SELECT id FROM user WHERE username LIKE '$username' ";
+
+            if ($results = mysqli_query($link, $query)) { // έλεγχος αν εκτελέστηκε επιτυχώς το ερώτημα στην βάση
+                $num_results = mysqli_num_rows($results);
+                if ($num_results > 0) {
+                    echo '<script type="text/javascript">' . 'alert("Υπάρχει ήδη λογαριασμός με αυτό το username!");' . '</script>';
+                } else {
+                    $query = "INSERT INTO user (username, password, first_name, last_name, email)
+                                      VALUES ('$username', '$password', '$firstname', '$lastname', '$email');";
+                    if ($results = mysqli_query($link, $query)) { // έλεγχος αν εκτελέστηκε επιτυχώς το ερώτημα στην βάση
+                        echo '<script type="text/javascript">' . 'closeForm1();' . '</script>';
+                        header("Location: Admin.php");
+                    }
+                }
+            }
+        }
+        if ($_POST['submit'] == 'Καταχώρηση δράσης') {
+            echo $_POST['submit'];
+            if (isset($_POST['title'])) {
+                $title = $_POST['title'];
+            } else {
+                $title = null;
+            }
+
+            if (isset($_POST['location'])) {
+                $location = $_POST['location'];
+            } else {
+                $location = null;
+            }
+            if (isset($_POST['link'])) {
+                $link_info = $_POST['link'];
+            } else {
+                $link_info = null;
+            }
+            if (isset($_POST['date'])) {
+                $date = $_POST['date'];
+            } else {
+                $date = null;
+            }
+            /*
+            $image = $_FILES['image']['tmp_name'];
+            $img = file_get_contents($image);
+            */
+            if (isset($_POST['img'])) {
+                $image = $_POST['img'];
+            } else {
+                $image = null;
+            }
+
+            if (isset($_POST['subject'])) {
+                $description = $_POST['subject'];
+            } else {
+                $description = null;
+            }
+
+            $query = "INSERT INTO action (title,date,location,description,image,link)
+                  VALUES ('$title','$date','$location','$description','$image','$link_info');";
+            if ($results = mysqli_query($link, $query)) { // έλεγχος αν εκτελέστηκε επιτυχώς το ερώτημα στην βάση
+                echo '<script type="text/javascript">' . 'closeForm2();' . '</script>';
+                header("Location: Admin.php");
+            }
+        }
+        @mysqli_free_result($results);
+        @mysqli_close($link);
+    }
+
+    ?>
 </head>
 <body>
 
@@ -185,57 +284,8 @@ include("connect_to_database.php");
             <label for="password"><b>Κωδικός</b></label>
             <input type="password" placeholder="Γράψε κωδικό" name="pass" required>
             </p>
-            <a href="Admin.php"><input type="submit" value="Δημιουργία" class="btn"/></a>
+            <input name="submit" type="submit" value="Καταχώρηση χρήστη" class="btn"/>
             <button type="button" class="btn_cancel" onclick="closeForm1()">Ακύρωση</button>
-
-            <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $link = 1; // άχρηστη γραμμή κώδικα, απλά για να μην εμφανίζει error στην μεταβλητή $link παρακάτω
-                include("connect_to_database.php");
-
-                if (isset($_POST['username'])) {
-                    $username = $_POST['username'];
-                } else {
-                    $username = null;
-                }
-                if (isset($_POST['email'])) {
-                    $email = $_POST['email'];
-                } else {
-                    $email = null;
-                }
-                if (isset($_POST['firstname'])) {
-                    $firstname = $_POST['firstname'];
-                } else {
-                    $firstname = null;
-                }
-                if (isset($_POST['lastname'])) {
-                    $lastname = $_POST['lastname'];
-                } else {
-                    $lastname = null;
-                }
-                if (isset($_POST['pass'])) {
-                    $password = $_POST['pass'];
-                } else {
-                    $password = null;
-                }
-
-                $query = "SELECT id FROM user WHERE username LIKE '$username' ";
-
-                if ($results = mysqli_query($link, $query)) { // έλεγχος αν εκτελέστηκε επιτυχώς το ερώτημα στην βάση
-                    $num_results = mysqli_num_rows($results);
-                    if ($num_results > 0) {
-                        echo '<script type="text/javascript">' . 'alert("Υπάρχει ήδη λογαριασμός με αυτό το username!");' . '</script>';
-                    } else {
-                        $query = "INSERT INTO user (username, password, first_name, last_name, email)
-                                  VALUES ('$username', '$password', '$firstname', '$lastname', '$email');";
-                        if ($results = mysqli_query($link, $query)) { // έλεγχος αν εκτελέστηκε επιτυχώς το ερώτημα στην βάση
-                            echo '<script type="text/javascript">' . 'closeForm1();' . '</script>';
-                        }
-                    }
-                }
-            }
-            ?>
-
         </form>
     </div>
 
@@ -331,8 +381,8 @@ include("connect_to_database.php");
     </div>
 
     <!-- pop up form για προσθήκη νέας δράσης από τον διαχειριστή -->
-    <div class="form-popup" id="myForm2" role="dialog">
-        <form action="/Admin.php" method="post" class="form-container">
+    <div class="form-popup" id="myForm2">
+        <form action="Admin.php" method="post" class="form-container">
             <h3>Δημιουργία δράσης</h3>
             <p>
                 <label for="title"><b>Τίτλος</b></label>
@@ -340,75 +390,26 @@ include("connect_to_database.php");
             </p>
             <p>
                 <label for="location"><b>Τοποθεσία</b></label>
-                <input type="text" placeholder="Δώσε τοποθεσία" name="location" required>
+                <input type="text" placeholder="Δώσε τοποθεσία" name="location">
             </p>
             <p>
                 <label for="link"><b>Link</b></label>
-                <input type="text" placeholder="Δώσε link" name="link">
+                <input type="url" placeholder="Δώσε link" name="link">
             </p>
             <p>
                 <label for="date"><b>Ημερομηνία</b></label>
-                <input type="date" placeholder="Δώσε ημερομηνία" name="date" required>
+                <input type="date" placeholder="Δώσε ημερομηνία" name="date">
             </p>
             <p>
                 <label for="image"><b>Εικόνα</b></label>
-                <input type="file" id="img" name="img" accept="image/*" placeholder="Δώσε εικόνα">
+                <input type="file" id="img" name="image" accept="image/*" placeholder="Δώσε εικόνα">
             </p>
             <p style="width: 100%">
                 <label for="subject"><b>Λεπτομέρειες</b></label>
                 <textarea placeholder="Δώσε περισσότερες πληροφορίες..." style="height:100px; width: 100%; resize: none;" name="subject" id="subject"></textarea>
             </p>
-            <a href="Admin.php"><input type="submit" value="Δημιουργία" class="btn"/></a>
+            <input name="submit" type="submit" value="Καταχώρηση δράσης" class="btn"/>
             <button type="button" class="btn_cancel" onclick="closeForm2()">Ακύρωση</button>
-
-            <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $link = 1; // άχρηστη γραμμή κώδικα, απλά για να μην εμφανίζει error στην μεταβλητή $link παρακάτω
-                include("connect_to_database.php");
-
-                if (isset($_POST['title'])) {
-                    $title = $_POST['title'];
-                } else {
-                    $title = null;
-                }
-                if (isset($_POST['location'])) {
-                    $location = $_POST['location'];
-                } else {
-                    $location = null;
-                }
-                /*
-                if (isset($_POST['link'])) {
-                    $link = $_POST['link'];
-                } else {
-                    $link = null;
-                }
-                */
-                if (isset($_POST['date'])) {
-                    $date = $_POST['date'];
-                } else {
-                    $date = null;
-                }
-                /*
-                if (isset($_POST['img'])) {
-                    $image = $_POST['img'];
-                } else {
-                    $image = null;
-                }
-                */
-                if (isset($_POST['subject'])) {
-                    $description = $_POST['subject'];
-                } else {
-                    $description = null;
-                }
-
-                $query = "INSERT INTO action (title, date, location, description)
-                          VALUES ('$title','$date','$location','$description');";
-                if ($results = mysqli_query($link, $query)) { // έλεγχος αν εκτελέστηκε επιτυχώς το ερώτημα στην βάση
-                    echo '<script type="text/javascript">' . 'closeForm2();' . '</script>';
-                }
-            }
-            ?>
-
         </form>
     </div>
 
@@ -475,6 +476,80 @@ include("connect_to_database.php");
         </table>
         <div class="table_page">Σελίδα 1/1</div>
     </div>
+
+    <h3>Επικοινωνία χρηστών</h3>
+    <div class="contact-table">
+        <p>ΟΛΕΣ ΟΙ ΦΟΡΜΕΣ
+            <?php //εμφανίζουμε το πλήθος των δράσεων
+            $link=1;
+            include("connect_to_database.php");
+            function print_size_of_table($link, $table){
+                //εμφανίζουμε το πλήθος των συνολικών συμμετοχών στις δράσεις
+                $query = "SELECT COUNT(*) FROM $table";
+                $results = mysqli_query($link, $query);
+                $row = mysqli_fetch_array($results);
+                echo '(' . $row[0] . ')';
+            }
+            print_size_of_table($link,'contact');
+            ?>
+            <button class="table_button">Ταξινόμηση</button>
+        </p>
+        <table>
+            <tr>
+                <th>id</th>
+                <th>Όνομα</th>
+                <th>Επίθετο</th>
+                <th>Email</th>
+                <th>Ημερομηνία</th>
+                <th class="keno"></th>
+            </tr>
+            <?php //εμφανίζουμε τον πίνακα όλων των δράσεων
+            $query = "SELECT id, first_name, last_name, email, comment, date_of_comment 
+                      FROM contact";
+            $results = mysqli_query($link, $query);
+            while ($row = mysqli_fetch_array($results)) {
+                echo '<tr>';
+                echo '<td>' . $row['id'] . '</td>';
+                echo '<td>' . $row['first_name'] . '</td>';
+                echo '<td>' . $row['last_name'] . '</td>';
+                echo '<td>' . $row['email'] . '</td>';
+                echo '<td>' . $row['date_of_comment'] . '</td>';
+                echo "<td class='keno'>
+                    <a href='javascript:void(0);' onclick='openForm3()'>Read</a>
+                </td>";
+                echo '</tr>';
+            }
+
+            @mysqli_free_result($results);
+            ?>
+        </table>
+        <div class="table_page">Σελίδα 1/1</div>
+    </div>
+
+    <!-- pop up form για προσθήκη νέας δράσης από τον διαχειριστή -->
+    <div class="form-popup" id="myForm3">
+        <form action="Admin.php" class="form-container">
+            <h3>Προβολή φόρμας</h3>
+            <?php
+                echo "Όνομα:";
+                echo "Επίθετο:";
+                echo "Email:";
+                echo "Ημερομηνία:";
+                echo "Σχόλια:";
+            ?>
+            <button type="button" class="btn_cancel" onclick="closeForm3()">κλείσιμο</button>
+        </form>
+    </div>
+
+    <script>
+        function openForm3() {
+            document.getElementById("myForm3").style.display = "block";
+        }
+        function closeForm3() {
+            document.getElementById("myForm3").style.display = "none";
+        }
+    </script>
+
 </div>
 
 <!-----------------Go to top button----------------->
